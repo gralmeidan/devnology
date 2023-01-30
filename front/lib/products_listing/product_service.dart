e962bea1fddc5f5346df1d4cf29ec891b4d33409
gralmeidan <gralmeidan@gmail.com>
2023-01-30 10:55:33 -0400

    feat(front): added ProductService class to fetch products from both apis

---
+++ front/lib/products_listing/product_service.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:front/products_listing/product.dart';
+import 'package:http/http.dart' as http;
+
+class ProductService {
+  Future<List<Product>> _fetchAllEuropean() async {
+    final response = await http.get(Uri.parse(
+        "http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/european_provider"));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)
+          .map((p) => Product.fromEuropeanProvider(p));
+    }
+
+    throw Exception("Failed to fetch from European Provider");
+  }
+
+  Future<List<Product>> _fetchAllBrazilian() async {
+    final response = await http.get(Uri.parse(
+        "http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/brazilian_provider"));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)
+          .map((p) => Product.fromBrazilianProvider(p));
+    }
+
+    throw Exception("Failed to fetch from Brazilian Provider");
+  }
+
+  Future<List<Product>> fetchAll() async {
+    final brazilianProducts = _fetchAllBrazilian();
+    final europeanProducts = _fetchAllEuropean();
+
+    final products = await Future.wait([brazilianProducts, europeanProducts]);
+
+    return products.expand((p) => p).toList();
+  }
+}
