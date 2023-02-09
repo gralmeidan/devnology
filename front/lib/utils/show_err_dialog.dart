3464d1daabd12d95ffe69d148d0865f410cfff90
gralmeidan <gralmeidan@gmail.com>
2023-02-09 17:07:41 -0400

    chore(front): added new showErrDialog utility function to display errors

---
+++ front/lib/utils/show_err_dialog.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+Future<void> showErrDialog({
+  required BuildContext context,
+  required String message,
+  String title = 'Algo deu errado',
+}) async {
+  return showDialog(
+    context: context,
+    builder: (BuildContext ctx) {
+      return AlertDialog(
+        title: Text(title),
+        content: SingleChildScrollView(
+          child: Text(message),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+            child: const Text('Prosseguir'),
+          )
+        ],
+      );
+    },
+  );
+}
